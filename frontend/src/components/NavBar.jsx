21f9be7e135bbc3c6c321781d5ee5b47396a1c3b
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2025-05-05 21:43:56 -0300

    setting up daisy ui components + themes

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+export default function NavBar() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">Clients!</a>
+      </div>
+      <div className="navbar-center">
+        <div className="flex gap-2" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="input input-bordered w-48 md:w-auto"
+        />
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-primary">Button</a>
+      </div>
+    </div>
+  );
+}
