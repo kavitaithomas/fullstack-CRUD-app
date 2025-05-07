6fb80e784a3bdc84017cd5a78a779fc832b8122e
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2025-05-07 17:17:37 -0300

    full screen modal

--- client/src/components/ModalForm.jsx
+++ client/src/components/ModalForm.jsx
@@ -2,59 +2,44 @@ export default function ModalForm({ isOpen, onClose, mode, onSubmit }) {
   return (
     <>
       <dialog id="my_modal_3" className="modal" open={isOpen}>
-        <div className="modal-box">
+        <div className="w-[90vw] h-[90vh] max-w-none p-4 bg-dark rounded-2xl overflow-y-auto">
           <h3 className="font-bold text-lg py-4">
             {mode === "edit" ? "Edit Client" : "Client Details"}
           </h3>
 
-          <div className="flex flex-col gap-y-2">
-            <div className="flex flex-row gap-x-2">
-              {" "}
-              <form method="dialog">
-                <div className="flex flex-col gap-y-2">
-                  <label className="input">
-                    Name
-                    <input
-                      type="text"
-                      className="grow"
-                      placeholder="Type Here"
-                    />
-                  </label>
-                  <label className="input">
-                    Email
-                    <input
-                      type="text"
-                      className="grow"
-                      placeholder="Type Here"
-                    />
-                  </label>
-                  <label className="input">
-                    Company
-                    <input
-                      type="text"
-                      className="grow"
-                      placeholder="Type Here"
-                    />
-                  </label>
-                  <label className="input">
-                    Job
-                    <input
-                      type="text"
-                      className="grow"
-                      placeholder="Type Here"
-                    />
-                  </label>
-                  <button
-                    className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 "
-                    onClick={onClose}
-                  >
-                    ✕
-                  </button>
-                </div>
-              </form>
-              <div className="">
-                <textarea className="textarea" placeholder="Notes"></textarea>
+          <div className="flex flex-col gap-y-4">
+            {" "}
+            <form method="dialog">
+              <div className="flex flex-row gap-x-4">
+                <label className="input">
+                  Name
+                  <input type="text" className="grow" placeholder="Type Here" />
+                </label>
+                <label className="input">
+                  Email
+                  <input type="text" className="grow" placeholder="Type Here" />
+                </label>
+                <label className="input">
+                  Company
+                  <input type="text" className="grow" placeholder="Type Here" />
+                </label>
+                <label className="input">
+                  Job
+                  <input type="text" className="grow" placeholder="Type Here" />
+                </label>
+                <button
+                  className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 "
+                  onClick={onClose}
+                >
+                  ✕
+                </button>
               </div>
+            </form>
+            <div className="">
+              <textarea
+                className="textarea w-full"
+                placeholder="Notes"
+              ></textarea>
             </div>
             <button className="btn btn-success">Save Changes</button>
           </div>
